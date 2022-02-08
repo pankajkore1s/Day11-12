b8b04d49d793a675fa5b36f110fefb27d2382bbd
pankaj kore <korep95@gmail.com>
2022-02-08 22:08:15 +0530

    [pankaj]added oops prog

---
+++ src/com/bridgeLabz/stockManagement/FIleConnection.java
@@ -0,0 +1,5 @@
+package com.bridgeLabz.stockManagement;
+
+public class FIleConnection {
+
+}
---
+++ src/com/bridgeLabz/stockManagement/StockAccountManagement.java
@@ -0,0 +1,22 @@
+package com.bridgeLabz.stockManagement;
+import java.util.List;
+import java.util.Scanner;
+import java.io.IOException;
+public class StockAccountManagement {
+	public static void main(String[] args) throws IOException  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1 : addStock");
+		System.out.println(" 2: Delete Stock");
+		System.out.println("3: value Of each Stock");
+		System.out.println("4 : Total Stock value");
+		int ch = sc.nextInt();
+		StockOperation op = new StockOperation();
+
+		do {
+			switch(ch) {
+			case 1:
+			}
+		{
+	}
+}
+	}
